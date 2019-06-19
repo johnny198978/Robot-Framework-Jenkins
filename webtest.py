505ad7d7951b29a4f7572c78ebfbb09671e507e1
johnny.lin <johnny.lin@insyde.com>
2019-06-19 15:47:20 +0800

    chrome option setting modify

--- webtest.py
+++ webtest.py
@@ -12,23 +12,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 #from test.init import ip
 import time
 import commands
-'''
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('--disable-gpu')
-'''
+
 class WebTest:
     def __init__(self):
         pass
 
     def WebLoginFWupdate(self,ip):
-        driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get("https://"+str(ip)+"/cgi/url_redirect.cgi?url_name=mainmenu")
         time.sleep(1)
 
