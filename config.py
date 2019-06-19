78544f022e7593c52622c9faeccbcf4de1c5e7b2
johnny.lin <johnny.lin@insyde.com>
2019-06-19 14:10:54 +0800

    test1

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ipmi =      "ipmitool"
-host =      "-H 192.168.89.156"
+host =      "-H 192.168.89.179"
 interface = "-I lanplus"
 user =      "-U root"
 passwd =    "-P root"
-ip = "192.168.89.156"
+ip = "192.168.89.179"
