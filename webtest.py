9e75b6f74096f3b6e808de953ce0bdb00dc56736
johnny.lin <johnny.lin@insyde.com>
2019-06-20 09:52:11 +0800

    chrome option setting modify

--- webtest.py
+++ webtest.py
@@ -29,6 +29,7 @@ class WebTest:
         #chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument('--disable-gpu')
+        chrome_options.binary_location = '/usr/bin/chromedriver'
         #chrome_options.set_headless()
         driver = webdriver.Chrome(chrome_options=chrome_options)
         driver.get("https://"+str(ip)+"/cgi/url_redirect.cgi?url_name=mainmenu")
