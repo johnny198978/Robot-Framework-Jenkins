1df6ed91417271d17a3c690621b10836e2f65463
johnny.lin <johnny.lin@insyde.com>
2019-06-20 10:30:48 +0800

    chrome option setting modify

--- webtest.py
+++ webtest.py
@@ -25,13 +25,13 @@ class WebTest:
     def WebLoginFWupdate(self,ip):
         chrome_options = webdriver.ChromeOptions()
         #chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--window-size=1024x768")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--window-size=1024x768')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-gpu')
         #chrome_options.binary_location = '/usr/bin/chromedriver'
         chrome_options.set_headless()
-        driver = webdriver.Chrome(executable_path = '/usr/bin/chromedriver',chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=chrome_options)
         driver.get("https://"+str(ip)+"/cgi/url_redirect.cgi?url_name=mainmenu")
         time.sleep(1)
 
